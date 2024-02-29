89ff2947d37a2d77fb63f6fa8d6ca94dbc4a8dc2
kunioshah <kunioshah@gmail.com>
2024-02-29 14:12:40 -0800

    Put different parts together
    Added Shiya's boat sprite to the player character, added Esther's boundaries to the ocean, added Aditya K's movement, added a camera following, added an ocean background

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.05F;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
+
