4c252343c81db28b65811b6f20c21e7814b625b7
kunioshah <kunioshah@gmail.com>
2024-02-29 14:46:48 -0800

    Created new camera
    Created a second camera system, it's still kind of janky and I need to mess around with it but I'm committing it for now (we will still use the main camera system until I figure it out though)

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
     public Transform target;
-    public float smoothTime = 0.05F;
+    public float smoothTime = 0.1f;
+    public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
 
     private void Start()
@@ -13,13 +14,24 @@ public class CameraMover : MonoBehaviour
 
     }
 
-    void Update()
+    private void LateUpdate()
     {
         // Define a target position above and behind the target transform
-        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+        Vector3 targetPosition = target.position + offset;
+        targetPosition.z = transform.position.z;
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
+
+    //void Update()
+    //{
+    //    // Define a target position above and behind the target transform
+    //    Vector3 targetPosition = target.position + offset;
+    //    targetPosition.z = transform.position.z;
+
+    //    // Smoothly move the camera towards that target position
+    //    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    //}
 }
 
