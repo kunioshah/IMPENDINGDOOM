d90463a4ee9ec649f2b84427680aab5a11d2e808
kunioshah <kunioshah@gmail.com>
2024-02-29 16:40:26 -0800

    Added player turning
    Added the player sprite turning around while the player is moving (we still need to update movement)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,7 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rb;
 
     private float moveSpeed;
+    public float rotateSpeed;
     private float x;
     private float y;
      
@@ -20,14 +21,20 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMover();
+    }
+
+    void PlayerMover() {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetAxisRaw("Horizontal") != 0)
+        Vector2 movementDirection = new Vector2(x, y);
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
             rb.AddForce(new Vector2(x * moveSpeed, 0f), ForceMode2D.Impulse);
         }
-        else if(Input.GetAxisRaw("Vertical") != 0)
+        else if (Input.GetAxisRaw("Vertical") != 0)
         {
             rb.AddForce(new Vector2(0f, y * moveSpeed), ForceMode2D.Impulse);
         }
@@ -35,5 +42,13 @@ public class PlayerMove : MonoBehaviour
         {
             rb.velocity = Vector2.zero;
         }
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+            
     }
+
 }
