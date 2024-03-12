3f0981fe6571dc0b553c354f141779606734ea0e
Aditya Kolekar <128189377+HeadlessSnow786@users.noreply.github.com>
2024-03-12 12:57:24 -0700

    Added Fish Pickup
    You are able to go over the fish and hit space to pick up the fish.

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UIElements;
+
+
+public class Fish : MonoBehaviour
+{
+    [SerializeField]
+    private bool pickupAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pickupAllowed && Input.GetKeyDown(KeyCode.Space))
+        {
+            pickUp();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Collect Sphere"))
+        {
+            pickupAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Collect Sphere"))
+        {
+            pickupAllowed = false;
+        }
+    }
+
+    private void pickUp()
+    {
+        Destroy(gameObject);
+    }
+}
