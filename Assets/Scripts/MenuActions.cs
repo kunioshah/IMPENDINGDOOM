c73e6c35eed482dbce9ef207723e3baecc8b33e7
Aditya Kolekar <128189377+HeadlessSnow786@users.noreply.github.com>
2024-03-19 21:06:09 -0700

    Added Basic UI
    I added the main menu with some functioning buttons. There is also a death screen that loads after the hunger bar drops to 0. Nothing is styled tho, but that can be done once we all decide on a theme.

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+  public void StartGame(){
+    SceneManager.LoadScene(1);
+  }
+  public void BackToMenu() {
+    SceneManager.LoadScene(0);
+  }
+  public void QuitGame() {
+    Application.Quit();
+  }
+}
