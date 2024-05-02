c3cda97538c433434b4ace33169a22514730c198
Wsther <143017872+Wsther@users.noreply.github.com>
2024-05-02 16:22:02 -0700

    Fixing the Fishy Dex
    Added text, new images that blend with the background, and a script to make it open.

---
+++ Assets/Scripts/DexOpen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DexOpen : MonoBehaviour
+{
+    private bool isDexOpen;
+    public GameObject dexCanvas;
+    public Button dexButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDexOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dexButton.onClick.AddListener(taskOnClick);
+    }
+private void taskOnClick ()
+    {
+        if (isDexOpen == false)
+        {
+            dexCanvas.SetActive(true);
+            isDexOpen = true;
+        }
+        else
+        {
+            dexCanvas.SetActive(false);
+            isDexOpen = false;
+        }
+    }
+}
