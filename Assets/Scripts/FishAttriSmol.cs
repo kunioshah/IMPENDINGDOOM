0a6abf5c32f4b773fcc88cd211c9c212d755ae32
Wsther <143017872+Wsther@users.noreply.github.com>
2024-05-04 13:25:14 -0700

    Add cash value to fish
    Add cash value to fish

---
+++ Assets/Scripts/FishAttriSmol.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAttriSmol : MonoBehaviour
+{
+    public GameObject smolFish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void attributePicker()
+    {
+        float[] fishValue = new float[] { 40, 50, 70, 10, 25, 60, 80, 50 };
+        if (smolFish)
+        {
+            float selfValue = Random.Range(fishValue[0], fishValue[2]);
+        }
+    }
+}
